9bb41204cd5a28844f23d20d9757d5118edebb5f
Jamie Bower <j_bower@hotmail.co.uk>
2024-07-30 21:29:47 +1000

    fix: got it working

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module ocr
 
 go 1.22.5
 
-require github.com/otiai10/gosseract v2.2.1+incompatible
-
-require (
-	github.com/otiai10/mint v1.6.3 // indirect
-	golang.org/x/net v0.27.0 // indirect
-)
+require github.com/otiai10/gosseract/v2 v2.4.1
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/otiai10/gosseract"
+	"github.com/otiai10/gosseract/v2"
 )
 
 func main() {
@@ -13,12 +13,18 @@ func main() {
 	client := gosseract.NewClient()
 	defer client.Close()
 
-	client.SetImage(filename)
-	text, err := client.Text()
-
+	err := client.SetImage(filename)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error performing set image on client: %v", err)
+
+	}
+
+	text, err := client.Text()
+	if err != nil {
+		// log.Fatal(err)
+		log.Fatalf("Error performing OCR: %v", err)
 	}
 
 	fmt.Println(text)
+
 }
