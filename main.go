c47be1811e9845ff6851c7c7d02436687cb2deb9
Jamie Bower <j_bower@hotmail.co.uk>
2024-08-04 15:13:42 +1000

    feat: send json response as base64 string of image data

--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"ocr/utils"
+	"os"
 
 	"github.com/otiai10/gosseract/v2"
 )
 
-func handler(w http.ResponseWriter, r * http.Request) {
-	fmt.Fprintf(w, "Hello from the winter air\n")
-}
-
-func main() {
-	sum := utils.Add(4, 5)
-	fmt.Println(sum)
-
-	var filename string = "./test_text.png"
-	
+func ImageToText(filename string) (string, error) {
 	client := gosseract.NewClient()
 	defer client.Close()
 
@@ -32,8 +26,62 @@ func main() {
 		log.Fatalf("Error performing OCR: %v", err)
 	}
 
-	fmt.Println(text)
+	return text, err
+}
 
-	http.HandleFunc("/", handler)
+func handleBasePath(w http.ResponseWriter, r * http.Request) {
+	fmt.Fprintf(w, "Hello from the Go web server")
+}
+
+func handleImagePath(w http.ResponseWriter, r * http.Request) {
+	text, err := ImageToText("./test_text.png")
+	if err != nil {
+		fmt.Fprintf(w, "Something went wrong")
+	}
+
+	fmt.Fprintf(w, text)
+}
+
+func readAndBase64EncodeImage(filepath string) (string, error) {
+	contents, err := os.ReadFile(filepath)
+
+	encoded := base64.StdEncoding.EncodeToString(contents)
+
+	return encoded, err
+}
+
+func handleEncodePath(w http.ResponseWriter, r * http.Request) {
+	encoded, err := readAndBase64EncodeImage("happy_birthday.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type ResponseData struct {
+		Data string `json:"data"`
+		Success bool `json:"success"`
+	}
+
+	response := ResponseData {
+		Data: encoded,
+		Success: true,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	encodeErr := json.NewEncoder(w).Encode(response)
+	if encodeErr != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+
+func main() {
+	sum := utils.Add(4, 5)
+	fmt.Println(sum)
+
+	http.HandleFunc("/", handleBasePath)
+	http.HandleFunc("/image", handleImagePath)
+	http.HandleFunc("/encode", handleEncodePath)
 	http.ListenAndServe(":8080", nil)
 }
