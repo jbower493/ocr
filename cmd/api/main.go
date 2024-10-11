bd1019b60c6db56c311d69534ff7d48a46338d83
Jamie Bower <j_bower@hotmail.co.uk>
2024-10-11 22:14:38 +1100

    feat: accept image in multipart form request, from main backend

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -68,7 +68,39 @@ func handleImageToTextPath(w http.ResponseWriter, r *http.Request) {
 	httpHelpers.HandleSuccessResponse(w, response)
 }
 
+func handleOptimizeImagePath(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusNotFound)
+		return
+	}
+
+	// Bitwise operator, shifts the bits of a number to the left, effectively multiplying it by 2. So this is like doing 10 * 2^20, which is aparently equal to 10MB (arg unit is bytes)
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	file, header, err := r.FormFile("file_to_optimize")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	mimeType := header.Header.Get("Content-Type")
+	param3 := r.FormValue("param3")
+	fmt.Println("Other field: ", param3)
+	fmt.Println("Mime type: ", mimeType)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	fmt.Fprintf(w, "Successfully failed lol")
+}
+
 func main() {
 	http.HandleFunc("/image-to-text", handleImageToTextPath)
+	http.HandleFunc("/optimize-image", handleOptimizeImagePath)
 	http.ListenAndServe(":8080", nil)
 }
