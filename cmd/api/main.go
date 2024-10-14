88aef440779d084ea92c3b3901a3831885007e43
Jamie Bower <j_bower@hotmail.co.uk>
2024-10-14 21:27:28 +1100

    fix: read exif data to account for rotated photos

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,8 @@ import (
 	"ocr/internal/resizeImage"
 
 	"github.com/chai2010/webp"
+	"github.com/disintegration/imaging"
+	"github.com/rwcarlsen/goexif/exif"
 )
 
 func handleOptimizeImagePath(w http.ResponseWriter, r *http.Request) {
@@ -34,30 +36,80 @@ func handleOptimizeImagePath(w http.ResponseWriter, r *http.Request) {
 	defer file.Close()
 
 	mimeType := header.Header.Get("Content-Type")
+	extension := strings.Split(mimeType, "/")[1]
 	// param3 := r.FormValue("param3") // get extra form fields out of the request
 
 	// Convert to webp
 	var img image.Image
 	var imageDecodeErr error
 
-	// PNG
-	// TODO: support other file types. Each one we decode, store the img and err in existing vars and just check them once after all if statements
+	// Get orientation
+	var orientationPointer *int
+
+	x, exifDecodeErr := exif.Decode(file)
+	if exifDecodeErr == nil {
+		orientation, orientationErr := x.Get(exif.Orientation)
+
+		if orientationErr == nil {
+			tempOrientationValue, tempOrientationValueErr := orientation.Int(0)
+
+			if tempOrientationValueErr == nil {
+				orientationPointer = &tempOrientationValue
+			}
+		}
+	}
+
+	// When decoding a file, it moves the file pointer forwards as data is read. So, because we've already decoded with with exif, we have to put the file pointer back to the start of the file
+	file.Seek(0, 0)
+
+	// Decode image
 	if mimeType == "image/png" {
 		img, imageDecodeErr = png.Decode(file)
 	} else if mimeType == "image/jpg" || mimeType == "image/jpeg" {
 		img, imageDecodeErr = jpeg.Decode(file)
 	} else {
-		http.Error(w, "."+strings.Split(mimeType, "/")[1]+" extention not supported", http.StatusBadRequest)
+		http.Error(w, extension+" extention not supported", http.StatusBadRequest)
 		return
 	}
 
 	if imageDecodeErr != nil {
-		http.Error(w, "Failed to decode png", http.StatusInternalServerError)
+		http.Error(w, "Failed to decode "+extension, http.StatusInternalServerError)
 		return
 	}
 
+	rotatedImg := img
+
+	// If we couldn't get the exif data just dont do any rotation
+	if orientationPointer != nil {
+		// Rotate image based on orientation
+		switch *orientationPointer {
+		// Rotate 180
+		case 3:
+			rotatedImg = imaging.Rotate180(img)
+		// Rotate 90° CW
+		case 6:
+			rotatedImg = imaging.Rotate270(img)
+		// Rotate 90° CCW
+		case 8:
+			rotatedImg = imaging.Rotate90(img)
+		// Normal
+		case 1:
+		// Flip horizontal
+		case 2:
+		// Flip vertical
+		case 4:
+		// Transpose (Rotate 90° CCW + Flip Horizontal)
+		case 5:
+		// Transverse (Rotate 90° CW + Flip Horizontal)
+		case 7:
+		// Unknown orientation
+		default:
+			rotatedImg = img
+		}
+	}
+
 	// Resize image so that the width is 500px and the height is auto to maintain the aspect ratio
-	resizedImg := resizeImage.ResizeImage(img)
+	resizedImg := resizeImage.ResizeImage(rotatedImg)
 
 	var buf bytes.Buffer
 
--- go.mod
+++ go.mod
@@ -6,3 +6,9 @@ require (
 	github.com/chai2010/webp v1.1.1
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 )
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+)
